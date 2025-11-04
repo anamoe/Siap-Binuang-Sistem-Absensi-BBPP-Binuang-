7c4ab60c7612c4192d398396d38db54b14354779
anamwoe <anam45188@gmail.com>
2025-11-04 11:58:47 +0800

    update

--- app/Http/Controllers/Api/TrackingPegawaiController.php
+++ app/Http/Controllers/Api/TrackingPegawaiController.php
@@ -141,35 +141,35 @@ class TrackingPegawaiController extends Controller
 
             ],
             'batasan_area' => [
-        
 
-                
-                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.154688343701509,   'longitude' => 115.08949961283274 ],
-                ['nama' => 'Batas Belakang Kanan0', 'latitude' =>-3.1545712375123256,   'longitude' => 115.08929662144064 ],
+
+
+                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.154688343701509,   'longitude' => 115.08949961283274],
+                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.1545712375123256,   'longitude' => 115.08929662144064],
                 ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.1545036762433547,   'longitude' => 115.08908460820888],
-                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.154351051710958,   'longitude' => 115.08890707939071 ],
+                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.154351051710958,   'longitude' => 115.08890707939071],
                 ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.1541407004601894,   'longitude' => 115.08876587115557],
-                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.1538193229207816,   'longitude' => 115.08863712512478 ],
-                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.153522048599311,   'longitude' => 115.08851374350668 ],
-                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.1534283134381424,  'longitude' => 115.08820260726556 ],//
-                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.153356003450897,   'longitude' =>115.0877949115013 ],
-                
-                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.153331900124682,  'longitude' => 115.08757765257262 ],
-                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.153256911982282,   'longitude' => 115.08680785858347 ],
-                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.1531122919704773,  'longitude' => 115.08637334071847 ],
-                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.153029269366863,   'longitude' => 115.08595491611834 ],
-                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.152973028248002,  'longitude' => 115.08574302160204 ],
-                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.152924821569288,  'longitude' => 115.08552844488402 ],
+                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.1538193229207816,   'longitude' => 115.08863712512478],
+                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.153522048599311,   'longitude' => 115.08851374350668],
+                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.1534283134381424,  'longitude' => 115.08820260726556], //
+                ['nama' => 'Batas Belakang Kanan0', 'latitude' => -3.153356003450897,   'longitude' => 115.0877949115013],
 
-                
-                ['nama' => 'Batas Depan Kiri0', 'latitude' => -3.1529649937978435,  'longitude' => 115.08538360559497 ],
-                ['nama' => 'Batas Depan Kiri0', 'latitude' => -3.152957031715876,   'longitude' =>115.08523651987295],
+                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.153331900124682,  'longitude' => 115.08757765257262],
+                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.153256911982282,   'longitude' => 115.08680785858347],
+                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.1531122919704773,  'longitude' => 115.08637334071847],
+                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.153029269366863,   'longitude' => 115.08595491611834],
+                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.152973028248002,  'longitude' => 115.08574302160204],
+                ['nama' => 'Batas Belakang Kiri0', 'latitude' => -3.152924821569288,  'longitude' => 115.08552844488402],
+
+
+                ['nama' => 'Batas Depan Kiri0', 'latitude' => -3.1529649937978435,  'longitude' => 115.08538360559497],
+                ['nama' => 'Batas Depan Kiri0', 'latitude' => -3.152957031715876,   'longitude' => 115.08523651987295],
                 ['nama' => 'Batas Depan Kiri0', 'latitude' => -3.1530324752448404,   'longitude' => 115.08510344773812],
                 ['nama' => 'Batas Depan Kiri0', 'latitude' => -3.153127061157416,   'longitude' => 115.084956842842547],
                 ['nama' => 'Batas Depan Kiri0', 'latitude' => -3.153267813985657,  'longitude' => 115.08477189512698],
-                
+
                 ['nama' => 'Batas Depan Kiri', 'latitude' => -3.153422954410393,  'longitude' => 115.08464420662081],
-                ['nama' => 'Batas Depan Kiri', 'latitude' => -3.1548898600455995,  'longitude' => 115.0851682555448 ],
+                ['nama' => 'Batas Depan Kiri', 'latitude' => -3.1548898600455995,  'longitude' => 115.0851682555448],
                 ['nama' => 'Batas Depan Kiri', 'latitude' => -3.1551848774538778,   'longitude' => 115.08526072940198],
                 ['nama' => 'Batas Depan Kiri', 'latitude' => -3.15541683764037,   'longitude' => 115.085350947798471],
                 ['nama' => 'Batas Depan Kiri', 'latitude' => -3.155666814097824,   'longitude' => 115.08548401993437],
@@ -177,19 +177,19 @@ class TrackingPegawaiController extends Controller
 
 
                 ['nama' => 'Batas Depan Kanan', 'latitude' => -3.156035929443358,  'longitude' => 115.08570517155849],
-                ['nama' => 'Batas Depan Kanan2', 'latitude' =>-3.1556555958537547,    'longitude' =>115.08622341135359] ,
-                ['nama' => 'Batas Depan Kanan3', 'latitude' =>-3.155450660204763,   'longitude' =>115.08649181108312  ],
-               
+                ['nama' => 'Batas Depan Kanan2', 'latitude' => -3.1556555958537547,    'longitude' => 115.08622341135359],
+                ['nama' => 'Batas Depan Kanan3', 'latitude' => -3.155450660204763,   'longitude' => 115.08649181108312],
 
-                ['nama' => 'Batas Tengah Kanan1', 'latitude' => -3.1553065296053644,  'longitude' => 115.08696320221812 ],
-                ['nama' => 'Batas Tengah Kanan2', 'latitude' => -3.155345279269797,  'longitude' => 115.08699689876804 ],
+
+                ['nama' => 'Batas Tengah Kanan1', 'latitude' => -3.1553065296053644,  'longitude' => 115.08696320221812],
+                ['nama' => 'Batas Tengah Kanan2', 'latitude' => -3.155345279269797,  'longitude' => 115.08699689876804],
                 ['nama' => 'Batas Tengah Kanan3', 'latitude' => -3.1555395560951918,    'longitude' => 115.0871436250599],
-                ['nama' => 'Batas Tengah Kanan4', 'latitude' => -3.155784790733491,   'longitude' =>115.0873445762872],
+                ['nama' => 'Batas Tengah Kanan4', 'latitude' => -3.155784790733491,   'longitude' => 115.0873445762872],
 
                 ['nama' => 'Batas Belakang Tengah', 'latitude' => -3.1560289548181393,   'longitude' => 115.08753323730369],
-                ['nama' => 'Batas Belakang Tengah1', 'latitude' => -3.1561556640135806,   'longitude' =>115.0876575506758],
-                
-                
+                ['nama' => 'Batas Belakang Tengah1', 'latitude' => -3.1561556640135806,   'longitude' => 115.0876575506758],
+
+
 
 
             ],
@@ -268,6 +268,7 @@ class TrackingPegawaiController extends Controller
                 'rtp.id',
                 'rtp.id_user',
                 'user.nama',
+                'user.foto_profil',
                 'pegawai.jabatan',
                 'pegawai.no_hp',
                 'rtp.latitude',
@@ -288,6 +289,13 @@ class TrackingPegawaiController extends Controller
             ->leftJoin('pegawai', 'user.id_pegawai', '=', 'pegawai.id_pegawai')
             ->orderByDesc('rtp.created_at')
             ->get();
+
+        $track->transform(function ($item) {
+
+            $fotoProfilUrl = asset('foto/' . $item->foto_profil);
+            $item->foto_profil = $fotoProfilUrl;
+            return $item;
+        });
 
         if ($track->isEmpty()) {
             return response()->json([
